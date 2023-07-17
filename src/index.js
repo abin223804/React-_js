553357c5b439767bdac11072114cf159893e530a
Abin s chandran <abinschandran@gmail.com>
2023-07-17 16:23:34 +0530

    base created

--- src/index.js
+++ src/index.js
@@ -1,14 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="app">
+    <div className="app_header">
+      <span>Home </span>
+      <span>Usage </span>
+      <span>Setting </span>
+      <span>Logout </span>
+    </div>
+    <div className="app_body">
+      <div className="app_list">
+        <div className="list_item">
+          <hr />
+          <div className="list_title">
+            <h4>
+             My Title One 
+            </h4>
+
+          </div>
+          <div className="list_descr">
+            This is a very big Description
+
+          </div>
+          <div className="list_label">
+            <span>Label1</span>
+            <span>Labe2</span>
+            <span>Label3</span>
+
+          </div>
+
+          <hr />
+          <div className="list_title">
+            <h4>
+             My Title One 
+            </h4>
+
+          </div>
+          <div className="list_descr">
+            This is a very big Description
+
+          </div>
+          <div className="list_label">
+            <span>Label1</span>
+            <span>Labe2</span>
+            <span>Label3</span>
+
+          </div>
+
+          <hr />
+          <div className="list_title">
+            <h4>
+             My Title One 
+            </h4>
+
+          </div>
+          <div className="list_descr">
+            This is a very big Description
+
+          </div>
+          <div className="list_label">
+            <span>Label1</span>
+            <span>Labe2</span>
+            <span>Label3</span>
+
+          </div>
+
+          <hr />
+        </div>
+      </div>
+    </div>
+    <div className="app_footer"></div>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
