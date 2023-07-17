8edebd5ac80e9fe86fc04877450a1b8aa2f4c99f
Abin s chandran <abinschandran@gmail.com>
2023-07-17 16:37:02 +0530

    component addedd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import reportWebVitals from "./reportWebVitals";
+function listitem() {
+  return (
+    <div className="list_item">
+      <hr />
+      <div className="list_title">
+        <h4>My Title One</h4>
+      </div>
+      <div className="list_descr">This is a very big Description</div>
+      <div className="list_label">
+        <span>Label1</span>
+        <span>Labe2</span>
+        <span>Label3</span>
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,71 +29,12 @@ root.render(
     </div>
     <div className="app_body">
       <div className="app_list">
-        <div className="list_item">
-          <hr />
-          <div className="list_title">
-            <h4>
-             My Title One 
-            </h4>
-
-          </div>
-          <div className="list_descr">
-            This is a very big Description
-
-          </div>
-          <div className="list_label">
-            <span>Label1</span>
-            <span>Labe2</span>
-            <span>Label3</span>
-
-          </div>
-
-          <hr />
-          <div className="list_title">
-            <h4>
-             My Title One 
-            </h4>
-
-          </div>
-          <div className="list_descr">
-            This is a very big Description
-
-          </div>
-          <div className="list_label">
-            <span>Label1</span>
-            <span>Labe2</span>
-            <span>Label3</span>
-
-          </div>
-
-          <hr />
-          <div className="list_title">
-            <h4>
-             My Title One 
-            </h4>
-
-          </div>
-          <div className="list_descr">
-            This is a very big Description
-
-          </div>
-          <div className="list_label">
-            <span>Label1</span>
-            <span>Labe2</span>
-            <span>Label3</span>
-
-          </div>
-
-          <hr />
+       
+        {listitem()}
+        {listitem()}
+        {listitem()}
         </div>
-      </div>
     </div>
     <div className="app_footer"></div>
   </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-// abin s chandran
