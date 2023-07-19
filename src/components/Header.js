a6931a95db0c6f2dc332dbd73b1c1ca46773eb23
Abin s chandran <abinschandran@gmail.com>
2023-07-19 12:30:37 +0530

    components added with header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css';
+
+function Header(){
+    return(
+        <div className="app_header">
+      <span>Home </span>
+      <span>Usage </span>
+      <span>Setting </span>
+      <span>Logout </span>
+    </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ListItem from './List/ListItem';
+import Header from "./components/Header";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className="app">
-    <div className="app_header">
-      <span>Home </span>
-      <span>Usage </span>
-      <span>Setting </span>
-      <span>Logout </span>
-    </div>
+    <Header/>
     <div className="app_body">
       <div className="app_list">
       <ListItem/>
